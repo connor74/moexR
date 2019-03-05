df37a7ba2a3284c59c786652452a1eb937c4144e
connor74 <kurzanovart@gmail.com>
2019-03-05 23:09:20 +0500

    Make function get_repo_by_date(date)

--- script.R
+++ script.R
@@ -3,45 +3,47 @@ library(xml2)
 library(lubridate)
 library(dplyr)
 
-
-
-k <- TRUE
-i = 1 # max row in the request
-while(k) {
-  path <- "http://iss.moex.com/iss/history/engines/stock/markets/ccp/trades.xml?date="
-  date <- "2019-03-04"
-  if(i == 1) {
-    link <- paste0(path, date)
-  } else {
-    link <- paste0(path, date, "&start=", as.character(i + 1))
-  }
-
-  xml <- read_xml(GET(link))
-  rows <- xml_find_all(xml, "//row")
+get_repo_by_date <- function(date) {
+  k <- TRUE
+  i = 1 # max row in the request
+  while(k) {
+    path <- "http://iss.moex.com/iss/history/engines/stock/markets/ccp/trades.xml?date="
+    dt <- as.character(date)
+    if(i == 1) {
+      link <- paste0(path, dt)
+    } else {
+      link <- paste0(path, dt, "&start=", as.character(i + 1))
+    }
   
-  if(length(rows) == 0) break
+    xml <- read_xml(GET(link))
+    rows <- xml_find_all(xml, "//row")
+    
+    if(length(rows) == 0) break
+    
+    if(is.null(dim(df))) {
+      df <- data.frame(
+        TRADEDATE = xml_attr(rows, "TRADEDATE"),
+        TRADETIME = xml_attr(rows, "REPORATE"),
+        BOARDID = xml_attr(rows, "BOARDID"),
+        SECID = xml_attr(rows, "SECID"),
+        REPORATE = xml_attr(rows, "REPORATE")
+      )    
+    } else {
+      df_add <- data.frame(
+        TRADEDATE = xml_attr(rows, "TRADEDATE"),
+        TRADETIME = xml_attr(rows, "REPORATE"),
+        BOARDID = xml_attr(rows, "BOARDID"),
+        SECID = xml_attr(rows, "SECID"),
+        REPORATE = xml_attr(rows, "REPORATE")
+      ) 
+      df <- rbind(df, df_add)
+      rm(df_add)
+    }
   
-  if(is.null(dim(df))) {
-    df <- data.frame(
-      TRADEDATE = xml_attr(rows, "TRADEDATE"),
-      TRADETIME = xml_attr(rows, "REPORATE"),
-      BOARDID = xml_attr(rows, "BOARDID"),
-      SECID = xml_attr(rows, "SECID"),
-      REPORATE = xml_attr(rows, "REPORATE")
-    )    
-  } else {
-    df_add <- data.frame(
-      TRADEDATE = xml_attr(rows, "TRADEDATE"),
-      TRADETIME = xml_attr(rows, "REPORATE"),
-      BOARDID = xml_attr(rows, "BOARDID"),
-      SECID = xml_attr(rows, "SECID"),
-      REPORATE = xml_attr(rows, "REPORATE")
-    ) 
-    df <- rbind(df, df_add)
-    rm(df_add)
-  }
-
-  i = i + 5000
+    i = i + 5000
+  }  
 }
+
+
 
 
